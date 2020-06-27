61a1c4627ad2a209c3c01e49149b1a4d186f35de
R.A. Nagy <r.a.nagy@gmail.com>
2020-06-27 15:38:33 -0400

    Testing Success.

---
+++ S9kGit/src/com/soft9000b/collections/ObjectEnumerator.java
@@ -0,0 +1,60 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2020 Randall.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.soft9000b.collections;
+
+import java.util.Enumeration;
+
+/**
+ * Yes, ObjectEnumerator is also part of FileDictionary. Yet another
+ * educational-discussion point.
+ *
+ * @param <T>
+ */
+public class ObjectEnumerator<T> implements Enumeration {
+
+    private final T[] data;
+    private int which = 0;
+
+    public ObjectEnumerator(T[] data) {
+        this.data = data;
+    }
+
+    @Override
+    public boolean hasMoreElements() {
+        if (data == null) {
+            return false;
+        }
+        return (data.length > this.which);
+    }
+
+    @Override
+    public Object nextElement() {
+        if (!hasMoreElements()) {
+            return null;
+        }
+        T result = data[which];
+        which += 1;
+        return result;
+    }
+}

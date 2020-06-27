11a049b4cefd0d978be4c09c9017a463037e9e65
R.A. Nagy <r.a.nagy@gmail.com>
2020-06-27 16:35:07 -0400

    Mostly Harmless ... Needs re-verification.

---
+++ S9kGit/src/com/soft9000b/file/TextLineReader.java
@@ -0,0 +1,131 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2020 Randall.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.soft9000b.file;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ * This OBJECT will read lines - and maintain the file handle - for a linear
+ * TEXT file.
+ *
+ * An opportunity to create a simple, line-oriented, string reader. This must be
+ * a 'weaponized' class whose interface must be relatively robust (i.e. it will
+ * be used a lot!)
+ *
+ * @author ProfNagy
+ */
+public class TextLineReader {
+
+    private File pwFile = null;
+    private BufferedReader in = null;
+
+    /**
+     * Create a 'slow reader' for a newline-encrusted file.
+     *
+     * @param file A newline-delimited file.
+     */
+    public TextLineReader(File file) {
+        pwFile = file;
+    }
+
+    /**
+     * See if the the this instance is ready to be used.
+     *
+     * @return TRUE if the buffers are open & ready to use. False if all is
+     * closed.
+     */
+    public boolean isOpen() {
+        return (in != null);
+    }
+
+    /**
+     * Tell us what we passed to the constructor -
+     *
+     * @return What was passed to the constructor -
+     */
+    public File getFile() {
+        return pwFile;
+    }
+
+    /**
+     * Open a file for reading.
+     *
+     * @return False on error / file n/f.
+     */
+    public boolean open() {
+        if (in != null) {
+            close();
+        }
+
+        if (pwFile.exists() == false) {
+            return false;
+        }
+
+        try {
+            FileReader reader = new FileReader(pwFile);
+            in = new BufferedReader(reader);
+        } catch (IOException e) {
+            in = null;
+            return false;
+        }
+        return true;
+
+    }
+
+    /**
+     * Reads a line from the file. Returns null on error or end of file.
+     *
+     * @return The line, or a null.
+     */
+    public String readLine() {
+        if (in == null) {
+            return null;
+        }
+        try {
+            return in.readLine();
+        } catch (IOException ex) {
+            return null;
+        }
+    }
+
+    /**
+     * Close the stream, if it is open. Can *always* be called.
+     */
+    public boolean close() {
+        if (in == null) {
+            return true;
+        }
+        try {
+            in.close();
+            return true;
+        } catch (IOException ex) {
+            return false;
+        } finally {
+            in = null;
+        }
+    }
+}
---
+++ S9kGit/src/com/soft9000b/file/TextLineWriter.java
@@ -0,0 +1,167 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2020 Randall.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.soft9000b.file;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+
+/**
+ * This OBJECT will write lines - and maintain the file handle - for a linear
+ * TEXT file.
+ *
+ * @author Professor Nagy
+ */
+public class TextLineWriter {
+
+    private File pwFile = null;
+    private BufferedWriter out = null;
+
+    /**
+     * Create a 'slow writer' for a newline-encrusted file. Will not be deleted
+     * or created until it is opened.
+     *
+     * @param file A newline-delimited file that you want to <b>over-write,
+     * delete, or create</b>.
+     *
+     */
+    public TextLineWriter(File file) {
+        pwFile = file;
+    }
+
+    /**
+     * Tell us what we passed to the constructor -
+     *
+     * @return What was passed to the constructor -
+     */
+    public File getFile() {
+        return pwFile;
+    }
+
+    /**
+     * Very useful for writing models form a common controller w/o munging
+     * states. Exceptions become a bonus.
+     *
+     * @return The BufferedWriter is and when open. Else null.
+     */
+    public BufferedWriter getStream() {
+        return out;
+    }
+
+    /**
+     * Open a file for writing. Will not be deleted until it is opened.
+     *
+     * @return False on error / file n/f.
+     */
+    public boolean open() {
+        return _open(false);
+    }
+
+    /**
+     * Open a file for appending to existing file. Existing file (if any) Will
+     * not be deleted.
+     *
+     * @return False on error / file n/f.
+     */
+    public boolean openAppend() {
+        return _open(true);
+    }
+
+    /**
+     * Writes a line to the file. Returns null on error or end of file.
+     *
+     * @return True if all went well.
+     */
+    public boolean writeLine(String str) {
+        if (out == null) {
+            return false;
+        }
+        try {
+            out.write(str);
+            out.newLine();
+            out.flush();
+            return true;
+        } catch (Exception ex) {
+            return false;
+        }
+    }
+
+    /**
+     * From time to time - like when processing xml - we need to simply add a
+     * block/line of text, assuming that it already HAS the required newlines.
+     * Normalization will NOT take place!
+     *
+     * @param str The NORMALIZED, NEWLINE DELIMITED line / bock of text.
+     * @return True if all went well
+     */
+    public boolean write(String str) {
+        if (out == null) {
+            return false;
+        }
+        try {
+            out.write(str);
+            out.flush();
+            return true;
+        } catch (Exception ex) {
+            return false;
+        }
+    }
+
+    /**
+     * Close the stream, if it is open. Can *always* be called.
+     */
+    public boolean close() {
+        if (out == null) {
+            return true;
+        }
+        try {
+            out.flush();
+            out.close();
+            return true;
+        } catch (Exception ex) {
+            return false;
+        } finally {
+            out = null;
+        }
+    }
+
+    private boolean _open(boolean bAppend) {
+        if (out != null) {
+            close();
+        }
+
+        if (bAppend == false && pwFile.exists()) {
+            pwFile.delete();
+        }
+
+        try {
+            FileWriter reader = new FileWriter(pwFile, bAppend);
+            out = new BufferedWriter(reader);
+        } catch (Exception e) {
+            out = null;
+            return false;
+        }
+        return true;
+    }
+}

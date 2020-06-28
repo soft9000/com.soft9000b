631868fa8600f35d36da479ffb7f9db0b45c7f45
R.A. Nagy <r.a.nagy@gmail.com>
2020-06-28 08:26:25 -0400

    Testing Success.

--- S9kGit/test/com/soft9000b/collections/FileDictionaryTest.java
+++ S9kGit/test/com/soft9000b/collections/FileDictionaryTest.java
@@ -1,12 +1,35 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * The MIT License
+ *
+ * Copyright 2020 Randall.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  */
 package com.soft9000b.collections;
 
 import com.soft9000b.tv.TagPair;
+import java.io.File;
 import java.util.Enumeration;
+import org.junit.After;
+import org.junit.AfterClass;
+// import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 /**
@@ -23,12 +46,57 @@ public class FileDictionaryTest {
         "Four", "Five", "Six"
     };
 
-    String sFileName = "~test.txt";
+    static String TEST_FILE = "~test.txt";
 
     final FileDictionary zFile;
 
     public FileDictionaryTest() {
-        zFile = new FileDictionary(sFileName);
+        zFile = new FileDictionary(TEST_FILE);
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+        {
+            File file = new File(FileDictionary.DEFAULT_FILE);
+            if (file.exists()) {
+                file.delete();
+            }
+        }
+        {
+            File file = new File(TEST_FILE);
+            if (file.exists()) {
+                file.delete();
+            }
+
+        }
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+        {
+            File file = new File(FileDictionary.DEFAULT_FILE);
+            if (file.exists()) {
+                file.delete();
+            }
+        }
+        {
+            File file = new File(TEST_FILE);
+            if (file.exists()) {
+                file.delete();
+            }
+
+        }
+    }
+
+/*
+    @Before
+    public void setUp() {
+
+    }
+*/
+    @After
+    public void tearDown() {
+        dePopulate();
     }
 
     /**
